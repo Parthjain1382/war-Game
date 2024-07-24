1b9fa04ca41017a14b7780d897fc5fc5d8ce86fb
Pranay <pranay.jain@innovapptive.com>
2024-07-24 12:46:36 +0530

    Initial Commit

---
+++ WarGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WarGame
+//
+//  Created by E5000846 on 24/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WarGame/WarGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarGameApp.swift
+//  WarGame
+//
+//  Created by E5000846 on 24/07/24.
+//
+
+import SwiftUI
+
+@main
+struct WarGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
