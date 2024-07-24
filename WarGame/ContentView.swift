4b4ebb515b298770d18e7f3d41536cb49478423f
Pranay <pranay.jain@innovapptive.com>
2024-07-24 18:08:22 +0530

    War Game Using SwiftUi

--- WarGame/ContentView.swift
+++ WarGame/ContentView.swift
@@ -8,17 +8,85 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var playerCard = "card-back"
+    @State var computerCard = "card-back"
+    @State var playerScore = 0
+    @State var computerScore = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            
+            Image("background")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .ignoresSafeArea()
+            
+            VStack{
+                Image("logo")
+                
+                
+                Spacer()
+                
+                HStack{
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(computerCard)
+                    Spacer()
+                }.padding(.all)
+                
+                Spacer()
+                
+                Button(action: {
+                   
+                    let playerNo = randomImageGenerate()
+                    let ComputerNo = randomImageGenerate()
+                    playerCard = "card-" + "\(playerNo)"
+                    computerCard = "card-" + "\(ComputerNo)"
+                    
+                    if(playerNo > ComputerNo){
+                        playerScore += 1
+                    }else if(playerNo < ComputerNo){
+                        computerScore += 1
+                    }
+                    
+                }, label: {
+                    Image("deal-button")
+                })
+                
+                Spacer()
+                
+                HStack{
+                    Spacer()
+                    VStack{
+                        Text("Player 1")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                    VStack{
+                        Text("Player 2")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text(String(computerScore))
+                            .font(.largeTitle )
+                    }
+                    Spacer()
+                }.foregroundColor(.white)
+            }.padding(100.0)
         }
-        .padding()
+    }
+    
+    func randomImageGenerate() -> Int{
+        let randNo = Int.random(in: 2...14)
+        
+        return randNo
+        
     }
 }
-
+  
 #Preview {
     ContentView()
 }
